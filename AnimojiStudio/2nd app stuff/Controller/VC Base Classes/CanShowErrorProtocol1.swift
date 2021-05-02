336c2dc62912576949d073ff586f1efd279c4e9b
snehal <mulchandanisnehal1@gmail.com>
2021-05-02 13:21:04 -0700

    added canshowerror Protocol

---
+++ AnimojiStudio/2nd app stuff/Controller/VC Base Classes/CanShowErrorProtocol1.swift
@@ -0,0 +1,11 @@
+//
+//  CanShowErrorProtocol1.swift
+//  AnimojiStudio
+//
+//  Created by Snehal Mulchandani on 5/2/21.
+//
+
+import Foundation
+protocol CanShowErrorProtocol {
+    func showError(error: String)
+}
